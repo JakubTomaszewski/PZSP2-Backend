a827692fd477510b4252828a262b08270003a663
Filip Wiśniewski <fi.wisniew@gmail.com>
2022-04-18 19:56:11 +0200

    PZSP2-31 Feat: Create UserController
    for mapping URL requests and calling UserService methods

---
+++ src/main/java/com/pzsp2/user/UserController.java
@@ -0,0 +1,26 @@
+package com.pzsp2.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/all")
+    List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+}
