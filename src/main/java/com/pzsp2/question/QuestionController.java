d6915af2b428dba04d07c4c025a742d2a0e4efac
JakubTomaszewski <jakub.k.tomaszewski@gmail.com>
2022-04-24 19:54:13 +0200

    Fix: Fix error with cross-platform communication

--- src/main/java/com/pzsp2/question/QuestionController.java
+++ src/main/java/com/pzsp2/question/QuestionController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Date;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(path = "api/questions")
 public class QuestionController {
