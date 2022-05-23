370d298a653ccef926ece972c4e7650ff2674227
Gimli037 <wiktor.kulesza037@gmail.com>
2022-05-23 02:02:32 +0200

    PZSP2-59 Fix: Removed special signs from test's password generator

--- src/main/java/com/pzsp2/test/TestPasswordGenerator.java
+++ src/main/java/com/pzsp2/test/TestPasswordGenerator.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class TestPasswordGenerator {
     public static String generateRandomPassword(int len) {
         String chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijk"
-                + "lmnopqrstuvwxyz!@#$%&";
+                + "lmnopqrstuvwxyz";
         Random rnd = new Random();
         StringBuilder sb = new StringBuilder(len);
         for (int i = 0; i < len; i++)
